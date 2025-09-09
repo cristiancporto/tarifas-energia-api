ae4814aff96d84b30e9e35e7ec78e596c88b97a3
Cristian <github@cporto.dev>
2025-09-09 01:43:07 +0000

    added more months to bandeiras

--- data/bandeiras.js
+++ data/bandeiras.js
@@ -6,8 +6,23 @@ export default [
     },
     {
       mes: "2025-06",
-      tipo: "verde",
-      valor_adicional_kwh: 0.0
+      tipo: "vermelha patamar 1",
+      valor_adicional_kwh: 0.04463
+    },
+    {
+      mes: "2025-07",
+      tipo: "vermelha patamar 1",
+      valor_adicional_kwh: 0.04463
+    },
+    {
+      mes: "2025-08",
+      tipo: "vermelha patamar 2",
+      valor_adicional_kwh: 0.07877
+    },
+    {
+      mes: "2025-09",
+      tipo: "vermelha patamar 2",
+      valor_adicional_kwh: 0.07877
     }
   ];
   
